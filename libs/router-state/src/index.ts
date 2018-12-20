96d8bc629e91c022bd0f03fb56d8454c957fab75
jsonberry <jason.s.awbrey@gmail.com>
2018-12-20 08:12:32 -0800

    feat(pages): route to not-found page when there is a PageError

--- libs/router-state/src/index.ts
+++ libs/router-state/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/+state/router-state-serializer';
+export * from './lib/+state/router.actions';
 export * from './lib/+state/router.facade';
 export * from './lib/+state/router.selectors';
 export * from './lib/router-state.module';
