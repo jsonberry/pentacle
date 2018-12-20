12770731f62a34a22b904ff40d98168a917b8a80
jsonberry <jason.s.awbrey@gmail.com>
2018-12-20 08:12:32 -0800

    feat(pages): route to not-found page when there is a PageError

--- libs/pages-state/src/lib/+state/pages.effects.ts
+++ libs/pages-state/src/lib/+state/pages.effects.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { fromRouterActions } from '@pentacle/router-state';
+import { mapTo } from 'rxjs/operators';
+import { PagesActionTypes } from './pages.actions';
 
 @Injectable()
-export class PagesEffects {}
+export class PagesEffects {
+  @Effect()
+  pageNotFound$ = this.actions$.pipe(
+    ofType(PagesActionTypes.PageLoadError),
+    mapTo(new fromRouterActions.Go({ path: ['/404'] })),
+  );
+
+  constructor(private actions$: Actions) {}
+}
