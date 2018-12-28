06b6f7a0752da97a8a9e85e4a9c22c65b73e7318
jsonberry <jason.s.awbrey@gmail.com>
2018-12-28 10:24:07 -0800

    feat(pages): add titles

--- libs/introduction/src/lib/introduction/introduction.component.ts
+++ libs/introduction/src/lib/introduction/introduction.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PagesFacade } from '@pentacle/pages-state';
+import { shareReplay, pluck, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'pentacle-introduction',
@@ -7,6 +8,12 @@ import { PagesFacade } from '@pentacle/pages-state';
   styleUrls: ['./introduction.component.scss'],
 })
 export class IntroductionComponent {
-  introductionPageData$ = this.pagesFacade.page$('introduction');
+  introductionPageData$ = this.pagesFacade.page$('introduction').pipe(
+    filter(data => !!data),
+    shareReplay(),
+  );
+  title$ = this.introductionPageData$.pipe(pluck('title'));
+  content$ = this.introductionPageData$.pipe(pluck('content'));
+
   constructor(private pagesFacade: PagesFacade) {}
 }
