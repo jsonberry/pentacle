dc773ffb4c471a374ea796fb61146119f8d5e883
jsonberry <jason.s.awbrey@gmail.com>
2018-12-30 18:02:27 -0800

    feat: add Prism syntax highlighter
    Closes #58

--- libs/examples/retry-with-backoff/src/lib/retry-with-backoff/retry-with-backoff.component.ts
+++ libs/examples/retry-with-backoff/src/lib/retry-with-backoff/retry-with-backoff.component.ts
@@ -1,12 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { SyntaxHighlighterService } from '@pentacle/services';
 
 @Component({
   selector: 'pentacle-retry-with-backoff',
   templateUrl: './retry-with-backoff.component.html',
   styleUrls: ['./retry-with-backoff.component.scss'],
 })
-export class RetryWithBackoffComponent implements OnInit {
-  constructor() {}
+export class RetryWithBackoffComponent implements AfterViewInit {
+  example = this.syntaxHighlighter
+    .convertReservedHTMLEntities(`export const retryWithBackoff = ({
+    fallback = console.warn,
+    fallbackAction = null,
+    errorPassthrough = true,
+    retry = false,
+    opts = {
+      attempts: 9,
+      strategy: 'exponential',
+      timeout: 15000,
+    },
+  }: DataIntegrityCheck) => (stream$: any): Observable<any> =>
+    stream$.pipe(
+      retryWhen((errors$) =>
+        errors$.pipe(
+          timeout(opts.timeout || 15000),
+          zip(range(1, (opts.attempts || 9) + 1)),
+          mergeMap(([error, i]) => {
+            if (!retry || (opts.attempts || 9) < i) {
+              return throwError(error);
+            }
 
-  ngOnInit() {}
+            switch (opts.strategy || 'exponential') {
+              case 'linear':
+                return timer(i * 1000);
+
+              case 'exponential':
+                return timer(Math.pow(Math.E, i));
+            }
+          }),
+        ),
+      ),
+      catchError((err) => {
+        if (fallbackAction) {
+          return of(
+            errorPassthrough ? new fallbackAction(err) : new fallbackAction(),
+          );
+        }
+
+        if (typeof fallback === 'function') {
+          return of(errorPassthrough ? fallback(err) : fallback());
+        }
+
+        return of(fallback);
+      }),
+    );`);
+
+  constructor(private syntaxHighlighter: SyntaxHighlighterService) {}
+
+  ngAfterViewInit() {
+    this.syntaxHighlighter.highlightAll();
+  }
 }
