89bb9cd727fb56d35443e79abf92ac571db17afe
jsonberry <jason.s.awbrey@gmail.com>
2019-01-02 10:27:07 -0800

    feat(resources): add title to resource detail pages

--- libs/resources/src/lib/+state/resources.facade.ts
+++ libs/resources/src/lib/+state/resources.facade.ts
@@ -3,18 +3,26 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { select, Store } from '@ngrx/store';
 import { PostDetailDTO, State } from '@pentacle/models';
 import { isPostDetailDTO } from '@pentacle/posts-state';
-import { filter, map } from 'rxjs/operators';
+import { filter, map, pluck, shareReplay } from 'rxjs/operators';
 import { resourcesQuery } from './resources.selectors';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ResourcesFacade {
-  resourceByRoute$ = this.store.pipe(select(resourcesQuery.getResourceByRoute));
+  resourceByRoute$ = this.store.pipe(
+    select(resourcesQuery.getResourceByRoute),
+    shareReplay(),
+  );
   contentByRoute$ = this.resourceByRoute$.pipe(
     filter((post): post is PostDetailDTO => !!post && isPostDetailDTO(post)),
     map(post => this.sanitizer.bypassSecurityTrustHtml(post.content)), // this is for things like YouTube iframes
   );
+  titleByRoute$ = this.resourceByRoute$.pipe(
+    filter((post): post is PostDetailDTO => !!post && isPostDetailDTO(post)),
+    pluck('title'),
+  );
+
   resources$ = this.store.pipe(select(resourcesQuery.getResources));
 
   constructor(private store: Store<State>, private sanitizer: DomSanitizer) {}
