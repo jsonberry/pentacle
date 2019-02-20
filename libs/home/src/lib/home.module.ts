a0a65106c873739a0ef3dfdc54b616d48f416bd6
jsonberry <jason.s.awbrey@gmail.com>
2019-02-20 15:24:30 -0800

    feat: add cms link to home page

--- libs/home/src/lib/home.module.ts
+++ libs/home/src/lib/home.module.ts
@@ -3,12 +3,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { EffectsModule } from '@ngrx/effects';
+import { SharedUtilBypassSecurityTrustHtmlModule } from '@pentacle/shared/util-bypass-security-trust-html';
+import { SharedUtilCmsLinkModule } from '@pentacle/shared/util-cms-link';
 import { HomeEffects } from './+state/home.effects';
 import { HomeComponent } from './home/home.component';
 @NgModule({
   imports: [
     CommonModule,
     HttpClientModule,
+    SharedUtilBypassSecurityTrustHtmlModule,
+    SharedUtilCmsLinkModule,
     RouterModule.forChild([
       { path: '', pathMatch: 'full', component: HomeComponent },
     ]),
