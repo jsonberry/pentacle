df662bed523960c6512266241c3c4df0599b1848
jsonberry <jason.s.awbrey@gmail.com>
2019-02-22 22:57:08 -0800

    refactor: resource filters moved to modal

--- libs/resources-filter/src/lib/resources-filter.module.ts
+++ libs/resources-filter/src/lib/resources-filter.module.ts
@@ -1,16 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import {
+  ClrCheckboxModule,
+  ClrSignpostModule,
+  ClrModalModule,
+  ClrButtonModule,
+} from '@clr/angular';
 import { EffectsModule } from '@ngrx/effects';
 import { ResourcesFilterEffects } from './+state/resources-filter.effects';
 import { ResourcesFilterContainerComponent } from './resources-filter-container/resources-filter-container.component';
-import { ResourcesFilterComponent } from './resources-filter/resources-filter.component';
 import { ResourcesFilterDifficultiesSortPipe } from './resources-filter/resources-filter-difficulties-sort.pipe';
+import { ResourcesFilterComponent } from './resources-filter/resources-filter.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    ClrCheckboxModule,
+    ClrSignpostModule,
+    ClrModalModule,
+    ClrButtonModule,
     EffectsModule.forFeature([ResourcesFilterEffects]),
   ],
   exports: [ResourcesFilterContainerComponent],
