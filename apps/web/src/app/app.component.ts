c26ac513d582b9cf7571525265744c314633709c
jsonberry <jason.s.awbrey@gmail.com>
2019-02-26 21:29:05 -0800

    feat: add bug report links
    Closes #137

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -12,7 +12,157 @@ interface ScrollPositionRestore {
 
 @Component({
   selector: 'pentacle-root',
-  templateUrl: './app.component.html',
+  template: `
+    <clr-main-container>
+      <clr-header class="header-6">
+        <div class="branding">
+          <a routerLink="/" class="nav-link"
+            ><span class="title">Pentacle (Alpha)</span></a
+          >
+        </div>
+      </clr-header>
+      <div class="content-container">
+        <div class="content-area" #contentArea>
+          <pentacle-pages-loading-progress></pentacle-pages-loading-progress>
+          <div class="router-container"><router-outlet></router-outlet></div>
+        </div>
+        <clr-vertical-nav [clr-nav-level]="1">
+          <a
+            clrVerticalNavLink
+            routerLink="/introduction"
+            routerLinkActive="active"
+            ><clr-icon clrVerticalNavIcon shape="map-marker"></clr-icon
+            >Introduction</a
+          >
+          <a clrVerticalNavLink routerLink="/overview" routerLinkActive="active"
+            ><clr-icon clrVerticalNavIcon shape="cloud"></clr-icon>Overview</a
+          >
+          <a
+            clrVerticalNavLink
+            routerLink="/blueprint"
+            routerLinkActive="active"
+            ><clr-icon clrVerticalNavIcon shape="map"></clr-icon>Blueprint</a
+          >
+          <clr-vertical-nav-group routerLinkActive="active">
+            <clr-icon shape="flag" clrVerticalNavIcon></clr-icon>
+            Principles
+            <clr-vertical-nav-group-children>
+              <a
+                clrVerticalNavLink
+                routerLink="/principles/declare-what-instead-of-how"
+                routerLinkActive="active"
+              >
+                Declare What Instead of How
+              </a>
+              <a
+                clrVerticalNavLink
+                routerLink="/principles/restrict-each-module-to-one-purpose"
+                routerLinkActive="active"
+              >
+                Restrict Each Module to One Purpose
+              </a>
+              <a
+                clrVerticalNavLink
+                routerLink="/principles/think-of-everything-as-a-stream"
+                routerLinkActive="active"
+              >
+                Think of Everything as a Stream
+              </a>
+              <a
+                clrVerticalNavLink
+                routerLink="/principles/dont-store-what-you-can-derive"
+                routerLinkActive="active"
+              >
+                Don't Store What You Can Derive
+              </a>
+              <a
+                clrVerticalNavLink
+                routerLink="/principles/flow-data-in-one-direction"
+                routerLinkActive="active"
+              >
+                Flow Data in One Direction
+              </a>
+            </clr-vertical-nav-group-children>
+          </clr-vertical-nav-group>
+
+          <clr-vertical-nav-group routerLinkActive="active">
+            <clr-icon shape="blocks-group" clrVerticalNavIcon></clr-icon>
+            Modules
+            <clr-vertical-nav-group-children>
+              <a
+                clrVerticalNavLink
+                routerLink="/modules/store"
+                routerLinkActive="active"
+              >
+                Store
+              </a>
+              <a
+                clrVerticalNavLink
+                routerLink="/modules/command"
+                routerLinkActive="active"
+              >
+                Command
+              </a>
+              <a
+                clrVerticalNavLink
+                routerLink="/modules/query"
+                routerLinkActive="active"
+              >
+                Query
+              </a>
+              <a
+                clrVerticalNavLink
+                routerLink="/modules/facade"
+                routerLinkActive="active"
+              >
+                Facade
+              </a>
+              <a
+                clrVerticalNavLink
+                routerLink="/modules/proxy"
+                routerLinkActive="active"
+              >
+                Proxy
+              </a>
+              <a
+                clrVerticalNavLink
+                routerLink="/modules/view"
+                routerLinkActive="active"
+              >
+                View
+              </a>
+            </clr-vertical-nav-group-children>
+          </clr-vertical-nav-group>
+
+          <a
+            clrVerticalNavLink
+            routerLink="/resources"
+            routerLinkActive="active"
+            [queryParamsHandling]="'preserve'"
+          >
+            <clr-icon clrVerticalNavIcon shape="library"></clr-icon>Resources</a
+          >
+          <a clrVerticalNavLink routerLink="/examples" routerLinkActive="active"
+            ><clr-icon clrVerticalNavIcon shape="flask"></clr-icon>Examples</a
+          >
+          <div class="nav-divider"></div>
+          <a
+            clrVerticalNavLink
+            href="https://github.com/jsonberry/pentacle/issues/new?assignees=jsonberry&labels=bug&template=bug_report.md&title="
+            target="_blank"
+            ><clr-icon clrVerticalNavIcon shape="bug"></clr-icon>Report Bug</a
+          >
+          <a
+            clrVerticalNavLink
+            href="https://github.com/jsonberry/pentacle/issues/new?assignees=jsonberry&labels=content&template=content.md&title=Content+Issue"
+            target="_blank"
+            ><clr-icon clrVerticalNavIcon shape="pencil"></clr-icon>Content
+            Feedback</a
+          >
+        </clr-vertical-nav>
+      </div>
+    </clr-main-container>
+  `,
   styles: [
     `
       .main-container .content-container .content-area {
