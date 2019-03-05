2ce7fe03a633578e871285f01439b9b899af2bef
jsonberry <jason.s.awbrey@gmail.com>
2019-03-05 11:56:45 -0800

    chore: remove meta tags

--- libs/utils/src/lib/on-page-filter.ts
+++
@@ -1,7 +0,0 @@
-import { PageIds } from '@pentacle/models';
-import { filter } from 'rxjs/operators';
-
-export function onPageFilter(pageId: PageIds) {
-  return source$ =>
-    source$.pipe(filter((action: any) => pageId === action.page.id));
-}
